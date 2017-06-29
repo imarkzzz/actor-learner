a72f7779bd0ddcaebb15e59b6ba9ca007dc4ef6d
mark <1126314679@qq.com>
2017-06-29 11:36:00 +0800

    qunitify the speech recog

---
+++ algorithm/python/word_error.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def wer(r, h):
+  d = np.zeros((len(r)+1)*(len(h)+1), dtype=np.uint8)
+  d = d.reshape(len(r)+1, len(h)+1)
+
+  # initialization
+  for i in range(len(r)+1):
+    for j in range(len(h)+1):
+      if i == 0:
+        d[0][j] = j
+      elif j == 0:
+        d[i][0] = i
+  # computation
+  for i in range(1, len(r)+1):
+    for j in range(1, len(h)+1):
+      if r[i-1] == h[j-1]:
+        d[i][j] = d[i-1][j-1]
+      else:
+        substitution = d[i-1][j-1] + 1
+        insertion = d[i][j-1] + 1
+        deletion = d[i-1][j] + 1
+        d[i][j] = min(substitution, insertion, deletion)
+  
+  return d[len(r), len(h)]
+
+if __name__ == "__main__":
+  print(wer("who is there".split(), "is there".split()))
+  print(wer("who is there".split(), "".split()))
+  
+  # print(wer("abc".split(), "hijklmnopqrst".split()))
+  
+          
