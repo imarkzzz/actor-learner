74a7b94ed628cc8f5e6c754e80706c4d48e81d5a
iMarkzzz <1126314679@qq.com>
2019-01-30 15:16:37 +0800

    feat: big sum with int group

--- algorithm/java/src/datastructure/BigNumberSum.java
+++ algorithm/java/src/datastructure/BigNumberSum.java
@@ -1,5 +1,7 @@
 package datastructure;
 
+import java.math.BigInteger;
+
 public class BigNumberSum {
     /**
      * 大数求和(位的方式相加)
@@ -48,7 +50,74 @@ public class BigNumberSum {
         return sb.toString();
     }
 
+    /**
+     * 分组大整数加法
+     * @param a 大整数 a
+     * @param b 大整数 b
+     * @return
+     */
+    public static String getSumGroup(String a, String b) {
+        // 1. 把两个整数用数组分组，数组长度等于较大整数位数+1
+        int maxLen = a.length() > b.length() ? a.length() % 9 : b.length() % 9;
+        int[] arrayA = new int[maxLen + 1];
+        int k = maxLen;
+        for (int i = a.length() - 1; i >= 0; i-=9) {
+            StringBuilder sb = new StringBuilder();
+            int j = i - 8;
+            if (j<= 0) {
+                j = 0;
+            }
+            for (;j <= i; j++) {
+                sb.append(a.charAt(j));
+            }
+            arrayA[k] = Integer.parseInt(sb.toString());
+            k-=1;
+        }
+        int[] arrayB = new int[maxLen + 1];
+        k = maxLen;
+        for (int i = b.length() - 1; i >= 0; i-=9) {
+            StringBuilder sb = new StringBuilder();
+            int j = i - 8;
+            if (j<= 0) {
+                j = 0;
+            }
+            for (;j <= i; j++) {
+                sb.append(b.charAt(j));
+            }
+            arrayB[k] = Integer.parseInt(sb.toString());
+            k-=1;
+        }
+
+        // 2. 构建result数组，数组长度等于分组数
+        int[] result = new int[maxLen + 1];
+        // 3. 遍历数组，按组
+        for (int i = result.length - 1; i >= 0; i--) {
+            int temp = result[i];
+            temp += arrayA[maxLen - i];
+            temp += arrayB[maxLen - i];
+            // 判断是否进位
+            if (temp >= 1000_000_000) {
+                temp -= 1000_000_000;
+                result[i + 1] = 1;
+            }
+            result[i] = temp;
+        }
+        // 4. 把result数组再次逆序转成String
+        StringBuilder sb = new StringBuilder();
+        for (int i = result.length - 1; i >= 0; i--) {
+            sb.append(result[i]);
+        }
+        return sb.toString();
+    }
     public static void main(String[] args) {
-        System.out.printf(getSumBitWise("134348394839", "37438473289144738247"));
+        System.out.println(getSumBitWise("134348394839", "37438473289144738247"));
+        // 查看 int 类型最大的位数是10位数，第10位可以作为进位标记位(按位大数加法用到了int但是只算一位效率低)
+//        System.out.println(Integer.MAX_VALUE);
+        System.out.println(getSumGroup("134348394839", "37438473289144738247"));
+        // 和BigInteger的结果进行比较，检验结果是否正确
+        BigInteger bintA = new BigInteger("134348394839");
+        BigInteger bintB = new BigInteger("37438473289144738247");
+        BigInteger result = bintA.add(bintB);
+        System.out.println(result);
     }
 }
